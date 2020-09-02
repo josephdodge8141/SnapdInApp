9059955b0096580ce2d2cc2d8e7915cc99f21e7e
josephdodge8141 <josephdodge8141@gmail.com>
2020-09-01 20:31:00 -0600

    integration with asw

--- SnapdIn/index.js
+++ SnapdIn/index.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import Amplify from 'aws-amplify';
-import config from './aws-exports'
-import {AppRegistry} from 'react-native';
+import config from '../aws-exports'
+import { AppRegistry } from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
